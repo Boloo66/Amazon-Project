87589a3ba7dd4ed483335ab098f6611557549f8a
Boloo66 <oguntadeemmanuel66@icloud.com>
2023-08-11 00:24:19 +0100

    Amazon click + cart + quantity display.

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
- products = [{
+ /* products = [{
     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
     rating:{
@@ -24,7 +24,16 @@
         count: 50
     },
     priceCent: 799
-}]
+},
+{
+    image: 'images/products/kitchen-paper-towels-30-pack.jpg',
+    name: 'kitchen paper towels',
+    rating:{
+        stars: 4.5,
+        count: 200
+    },
+    priceCent: 12000
+}]*/
 
 let displayHTML = '';
 products.forEach((product) => {
@@ -47,11 +56,11 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCent / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-select-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -67,14 +76,81 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-add-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`
 });
 document.querySelector('.js-product-grid').innerHTML = displayHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    
+    let {productId}  = button.dataset;
+    let matchingItem;
+    let clicked = false
+    let timeoutId;
+    
+    function addClick(){
+        let cartValue;
+        cart.forEach((item) => {
+
+        cartValue = document.querySelector(`.js-select-${productId}`).value || 1;
+        console.log(cartValue);
+
+        if(productId === item.id){
+                matchingItem = item;
+            }
+        })
+
+        if(matchingItem){
+            matchingItem.quantity += Number(cartValue) || 1;
+        }
+
+        else{
+            cart.push({id: productId,
+            quantity: Number(cartValue) || 1});
+        }
+        
+        let cartQuantity = 0;
+        // get cart quantity
+          cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        })
+        //console.log(button.dataset.productName);
+        //console.log(cart);
+        document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+        console.log(cart);
+      }
+    button.addEventListener('click', () => {
+      
+      if (!clicked){
+        addClick();
+        clearTimeout(timeoutId);
+        document.querySelector(`.js-add-${productId}`).classList.add('opacity-1');
+        timeoutId = setTimeout(() => {
+          document.querySelector(`.js-add-${productId}`).classList.remove('opacity-1');
+        }, 2000);
+        clicked = true;
+      }
+      //console.log(cartValue);
+      else{
+        addClick();
+        clearTimeout(timeoutId);
+        document.querySelector(`.js-add-${productId}`).classList.add('opacity-1');
+        timeoutId = setTimeout(() => {
+          document.querySelector(`.js-add-${productId}`).classList.remove('opacity-1');
+        }, 2000);
+        
+        clicked = false;
+      }
+      
+        
+    })
+       
+});
